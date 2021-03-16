73f77dae2749c4c21f58855432f9551d9eee00ac
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2021-03-16 17:01:05 +0100

    Support page styled

--- client/src/components/pages/Support.js
+++ client/src/components/pages/Support.js
@@ -4,84 +4,48 @@ import './support.css';
 
 const Support = () => {
   return (
-    <Container>
-      <div>
-        <h1 className='text-center'> Welcome to Online Support </h1>
-      </div>
-      <div className='contact mb-2'>
-        <div className='row'>
-          <div className='col-md-3'>
-            <div className='contact-info'>
-              <img
-                src='https://image.ibb.co/kUASdV/contact-image.png'
-                alt='image'
-              />
-              <h2>Contact Us</h2>
-              <h4>We would love to hear from you !</h4>
-            </div>
-          </div>
-          <div className='col-md-9'>
-            <div className='contact-form'>
-              <div className='form-group'>
-                <label className='control-label col-sm-2' htmlFor='fname'>
-                  First Name:
-                </label>
-                <div className='col-sm-10'>
-                  <input
-                    type='text'
-                    className='form-control'
-                    id='fname'
-                    placeholder='Enter First Name'
-                    name='fname'
-                  />
+    <Container fluid className='support'>
+      <div className='support__content p-3'>
+        <div>
+          <h2 className='text-center text-white'> Welcome to Online Support </h2>
+        </div>
+        <div className='support__contact'>
+          <div className='row'>
+            <div className='col-lg-6 mx-auto'>
+              <div className='contact-form'>
+                <div className='form-group contact_email text-center'>
+                  <label className='control-label' htmlFor='email'>
+                    Already a customer? Contact support services
+                  </label>
+                  <div className=''>
+                    <input
+                      type='email'
+                      className='form-control'
+                      id='email'
+                      placeholder='Enter email'
+                      name='email'
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className='form-group'>
-                <label className='control-label col-sm-2' htmlFor='lname'>
-                  Last Name:
-                </label>
-                <div className='col-sm-10'>
-                  <input
-                    type='text'
-                    className='form-control'
-                    id='lname'
-                    placeholder='Enter Last Name'
-                    name='lname'
-                  />
+                <div className='form-group contact__textarea'>
+                  <p className='control-label text-center' htmlFor='comment'>
+                    <span className=''>Leave us a Comment:</span>
+                  </p>
+                  <div className='mx-auto'>
+                    <textarea
+                      className='form-control'
+                      rows={5}
+                      id='comment'
+                      defaultValue={''}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className='form-group'>
-                <label className='control-label col-sm-2' htmlFor='email'>
-                  Email:
-                </label>
-                <div className='col-sm-10'>
-                  <input
-                    type='email'
-                    className='form-control'
-                    id='email'
-                    placeholder='Enter email'
-                    name='email'
-                  />
-                </div>
-              </div>
-              <div className='form-group'>
-                <label className='control-label col-sm-2' htmlFor='comment'>
-                  Comment:
-                </label>
-                <div className='col-sm-10'>
-                  <textarea
-                    className='form-control'
-                    rows={5}
-                    id='comment'
-                    defaultValue={''}
-                  />
-                </div>
-              </div>
-              <div className='form-group'>
-                <div className='col-sm-offset-2 col-sm-10'>
-                  <button type='submit' className='btn btn-default'>
-                    Submit
-                  </button>
+                <div className='form-group'>
+                  <div className=''>
+                    <button type='submit' className='btn btn-outline-dark'>
+                      Submit
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
